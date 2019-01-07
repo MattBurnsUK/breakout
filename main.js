e2e07a2fab1e715b2e1b7b11eb55e7120c840f62
MattBurnsUk <hello@matt-burns.com>
2019-01-07 16:40:25 +0800

    initial commit with basic file setup

---
+++ main.js
@@ -0,0 +1,7 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
